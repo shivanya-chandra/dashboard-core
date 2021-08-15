dd2306d1d57cf23b019493af18e5185e1d0d3d4f
Daksh <dakshshrivastava2007@gmail.com>
2021-08-16 02:03:26 +0530

    done stuff

---
+++ dash/components/leftElem.js
@@ -0,0 +1,21 @@
+export default function LeftElements () {
+    return (
+        <div className="leftElements">
+        <span className="leftElements__time">
+            4:20pm
+        </span>
+        <span className='leftElements__auto'>
+            Auto
+        </span>
+        <span className='leftElements__battery box'>
+            69% Battery
+        </span>
+                <span className='leftElements__speed box'>
+            100km/hr
+        </span>
+                <span className='leftElements__maps box-2'>
+            map
+        </span>
+    </div>
+    )
+};
--- dash/pages/index.js
+++ dash/pages/index.js
@@ -1,10 +1,11 @@
 // import Head from 'next/head'
 // import styles from '../styles/Home.module.css'
+import LeftElements from "../components/leftElem";
 
 export default function Home() {
   return (
     <div>
-      <embed src="http://google.com" style={{width:"500", height: "300"}}/>
+      <LeftElements/>
     </div>
-  )
+  );
 }
